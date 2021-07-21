615010dc72ae804b2c5c9fa27e9f8d80c589dd6c
Lars <larsoeltjenbruns@gmail.com>
2021-07-22 00:55:22 +0200

    add redux-persists for localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-d
 import { Provider } from "react-redux";
 
 import { store } from "./redux/store";
-import Home from "./sites/Home";
+import { Home } from "./sites/Home";
 import { GlobalStyles } from "./global-styles";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Provider store={store}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Redirect to="/" />
-        </Switch>
+        <PersistGate loading={null} persistor={persistor}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </PersistGate>
       </Provider>
     </Router>
   );
