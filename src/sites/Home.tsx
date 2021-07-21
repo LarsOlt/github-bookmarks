09a535545274a3d4cba9d6c8446ece7a915723ee
Lars <larsoeltjenbruns@gmail.com>
2021-07-22 00:54:25 +0200

    add main site

--- src/sites/Home.tsx
+++ src/sites/Home.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { connect } from "react-redux";
 
-import { cardsActions } from "../redux/entities/cards";
 import { List } from "../components/List";
+import { NewList } from "../components/NewList";
 import { Header } from "../components/Header";
-import { RepositoryCard } from "../components/Cards/RepositoryCard";
+
+import { useAppSelector } from "../hooks/redux";
 
 const Styles = styled.div`
   height: 100%;
@@ -19,37 +19,24 @@ const Styles = styled.div`
     height: 100%;
     display: flex;
     align-items: flex-start;
+    align-content: flex-start;
     flex-wrap: wrap;
   }
 `;
 
-const Home: React.FC = (props) => {
-  console.log(props);
+export const Home: React.FC = () => {
+  const listIds = useAppSelector((state) => state.entities.lists.allIds);
 
   return (
     <Styles>
       <Header />
       <div className="main">
-        <List title="Javascript">
-          <RepositoryCard variant="ListItem" />
-          <RepositoryCard variant="ListItem" />
-          <RepositoryCard variant="ListItem" />
-        </List>
-        <List title="Node JS">
-          <RepositoryCard variant="ListItem" />
-          <RepositoryCard variant="ListItem" />
-        </List>
+        {listIds.map((id) => (
+          <List key={id} id={id} />
+        ))}
+
+        <NewList />
       </div>
     </Styles>
   );
 };
-
-const mapStateToProps = (state: any) => ({
-  cards: state.entities.cards,
-});
-
-const mapDispatchToProps = (dispatch: any) => ({
-  deleteCard: (payload: any) => dispatch(cardsActions.delete(payload)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
